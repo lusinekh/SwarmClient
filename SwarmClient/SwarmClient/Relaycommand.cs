79eaf882449ef7849aea3398f3a008cae7fb839a
khachatryan lusine <luskhachatryann@gmail.com>
2017-09-19 21:40:39 +0400

    com65

---
+++ SwarmClient/SwarmClient/Relaycommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace SwarmClient
+{
+    class Relaycommand : ICommand
+    {
+        #region Variables
+        private Action _ececute;
+        private Func<bool> _canececute;
+#endregion
+        #region Constructrs
+        public Relaycommand(Action ececute) : this(ececute, null)
+        { }
+        public Relaycommand(Action ececute,Func<bool> canececute)
+        {
+            _ececute = ececute;
+            _canececute = canececute;
+        }
+        #endregion
+        #region Propertys
+        public event EventHandler CanExecuteChanged
+        {
+            add {
+                if (_canececute != null)
+                    CommandManager.RequerySuggested += value;
+                }
+            remove
+            {
+                if (_canececute != null)
+                    CommandManager.RequerySuggested -= value;
+            }
+
+        }
+        #endregion
+        #region Functions
+        public bool CanExecute(object parameter)
+        {
+           return _canececute==null?true:_canececute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _ececute();
+        }
+#endregion
+    }
+}
