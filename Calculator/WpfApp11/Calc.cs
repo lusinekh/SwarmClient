190b3ae163dcab343089922f4329341a642be6fe
khachatryan lusine <lusin.khachatryan@mail.ru>
2018-07-23 11:40:33 +0400

    7

---
+++ Calculator/WpfApp11/Calc.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace WpfApp11
+{
+    internal class Calc
+    {
+        public static double converter(string text)
+        {
+            return Convert.ToDouble(text);
+
+        }
+
+        public static string converters(double text)
+        {
+            return Convert.ToString(text);
+
+        }
+        
+        public static string[] split(string text, string[] operetion)
+        {
+
+            return text.Split(operetion, StringSplitOptions.RemoveEmptyEntries);
+
+        }
+        public static string Containsop(string text, string[] operetion)
+
+        {
+            foreach (var in1 in operetion)
+
+            {
+
+                if (text.Contains(in1))
+                    return in1;
+            }
+
+            return null;
+        }
+
+        public static double calculator(string op,double number1,double number2)
+        {
+            double e=0;
+
+            if (op == "*")
+
+                e= number1 * number2;
+
+            else
+            if (op == "/")
+
+                e= number1 / number2;
+            else
+
+            if (op == "+")
+
+                e= number1 + number2;
+            else
+
+            if (op == "-")
+
+                e= number1 - number2;
+            else
+
+            if (op == "%")
+
+                e= (number1 * number2) / 100;
+
+
+            return e;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Calculator/WpfApp11/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            var tb = sender as Button;
+
+            textbox.Text += tb.Content as string;
+        }
+
+        private void Delete_Contex(object sender, RoutedEventArgs e)
+        {
+
+            textbox.Text = " ";
+
+        }
+
+        private void result(object sender, RoutedEventArgs e)
+        {
+            string op;
+            int op2 = 0;
+            string[] opList = {"*","/","+","-","%",".","&" };
+            string text = textbox.Text;
+
+            op= Calc.Containsop(text, opList);
+
+            string[] numberslist = Calc.split(text, opList);
+
+            double number1 = Calc.converter(numberslist[0]);
+
+            double number2 = Calc.converter(numberslist[1]);
+
+
+            textbox.Text += "=" + Calc.calculator(op, number1, number2);
+        }
+    }
+}
